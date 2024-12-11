874102a5e9954cd22f444f02c304448623eb2039
ANKIT RAI <ankitrai2762@gmail.com>
2024-12-11 19:39:16 +0530

    project update

--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -6,6 +6,13 @@ export const projectList = [
     githublink: "https://github.com/akkirai817/unit-converter",
     liveLink: "https://akkirai817.github.io/unit-converter/",
   },
+  {
+    para: "Quiz Web",
+    image: "/images/quizz.png",
+    tech: "html css javascript",
+    githublink: "https://github.com/akkirai817/Akki-QuizWeb",
+    liveLink: "https://akkirai817.github.io/Akki-QuizWeb/",
+  },
 
   {
     para: "Color Generator",
@@ -18,12 +25,16 @@ export const projectList = [
     para: "Speech-Text Generator",
     image: "/images/speechtext.png",
     tech: "HTML , CSS , JavaScript",
+    githublink: "https://github.com/akkirai817/speech-to-text",
+    liveLink: "https://speech-to-text-pearl.vercel.app/",
   },
 
   {
     para: "Weather Web",
     image: "/images/sc2.png",
     tech: "html css javascript",
+    githublink: "https://github.com/akkirai817/weather-web",
+    liveLink: "https://akkirai817.github.io/weather-web/",
   },
 
   {
@@ -38,22 +49,29 @@ export const projectList = [
     para: "PhoneFeatures Web",
     image: "/images/sc8.png",
     tech: "html css javascript",
+    liveLink: "https://akkirai817.github.io/phonefeature-web/",
+    githublink: "https://github.com/akkirai817/phonefeature-web",
   },
 
   {
     para: "timerandStopWatch",
     image: "/images/sc9.png",
     tech: "html css javascript",
+    githublink: "https://github.com/akkirai817/timer-stop-watch",
+    liveLink: "https://akkirai817.github.io/timer-stop-watch/",
   },
-  {
-    para: "temperatureConverter",
-    image: "/images/sc10.png",
-    tech: "html css javascript",
-  },
+
   {
     para: "Yumm Xxpress",
     image: "/images/yumm.png",
     tech: "html css javascript java ",
     githublink: "https://github.com/akkirai817/yum-project",
+  },
+  {
+    para: "E-commerce projec",
+    image: "/images/e.png",
+    tech: "html css javascript  ",
+    githublink: "https://github.com/akkirai817/collg-project",
+    liveLink: "https://akkirai817.github.io/collg-project/",
   },
 ];
